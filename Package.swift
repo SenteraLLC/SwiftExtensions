4535683893bab0508729d76905b2901d20e3337a
Bryan Rahn <bryan.rahn@sentera.com>
2021-07-19 16:15:04 -0500

    Convert to a Swift Package.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftExtensions",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "SwiftExtensions",
+            targets: ["SwiftExtensions"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftExtensions",
+            path: "Sources"),
+        .testTarget(
+            name: "SwiftExtensionsTests",
+            dependencies: ["SwiftExtensions"],
+            path: "Tests"),
+    ]
+)
